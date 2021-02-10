9fb6358d7f830c0b9cf01be01129bd3376ddce9f
David Andé <david.ande@wanadoo.fr>
2021-02-10 17:44:56 +0100

    Add files via upload

---
+++ Verif_MX_MIB.ps1
@@ -0,0 +1,11 @@
+﻿$Domaines = ipcsv -Path .\Domaines.csv -Delimiter ";" | Select -ExpandProperty Domaine -Unique
+Foreach ($Domaine in $Domaines) 
+    {
+        Resolve-DnsName -Name $Domaine -Type MX -DnsOnly |
+        Select Name, NameExchange, Preference |
+        Where Preference -GT 0 | Sort -Property Preference  |
+        Out-file .\checkMIB.txt -Append
+    }
+
+
+
